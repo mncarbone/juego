a40a0f3b868de7812f24722bdc0ce0ee7d986af6
unknown <mcarbone@Srv-Ds.ijmestrada.edu.ar>
2017-09-05 21:30:58 -0300

    Se muestra el saldo actualizado

--- Juego_qt.py
+++ Juego_qt.py
@@ -13,6 +13,8 @@ class JuegoQt(QApplication):
 
     def asociarEventos(self):
         self.ui.btn1.clicked.connect(self.btn1_click)
+        self.ui.btn2.clicked.connect(self.btn2_click)
+        self.ui.btn3.clicked.connect(self.btn3_click)
 
     def btn1_click(self):
         self.apostarPor(1)
@@ -28,6 +30,10 @@ class JuegoQt(QApplication):
         ganancia = self.juego.apostar(apuesta, unSombrero)
         salida = 'Ganó '+str(ganancia) if ganancia > 0 else 'Perdió'
         self.ui.lblSalida.setText(salida)
+        self.actualizarDatos()
+
+    def actualizarDatos(self):
+        self.ui.textSaldo.setText(str(self.juego.getSaldo()))
         
         
         
